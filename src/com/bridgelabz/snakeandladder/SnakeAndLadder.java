a2b2f6fa21b58011adebd429330c36f16b919f36
pra152 <pravatdas211@gmail.com>
2021-08-01 10:33:36 +0530

    Added : Checks for option

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,13 +1,80 @@
 package com.bridgelabz.snakeandladder;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 public class SnakeAndLadder {
-	private static final Scanner sc=new Scanner(System.in);
-	int players_position=0;
-	public static void main(String []args) {
-		System.out.println("Enter how many players:");
-		int players=sc.nextInt();
-	}
+	static int winpoint=100;
+	static Map < Integer , Integer > snake = new HashMap < Integer , Integer >();
+    static Map < Integer , Integer > ladder = new HashMap< Integer , Integer >();
+     
+    {
+        snake.put(99,54);
+        snake.put(70,55);
+        snake.put(52,42);
+        snake.put(25,2);
+        snake.put(95,72);
+         
+        ladder.put(6,25);
+        ladder.put(11,40);
+        ladder.put(60,85);
+        ladder.put(46,90);
+        ladder.put(17,69);
+    }
+        public static void main(String[] args) {
+        	SnakeAndLadder dice=new SnakeAndLadder();
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter Fisrt Player:");
+			int player=sc.nextInt();
+			int rolldice=dice.roll();
+			System.out.println("Roll the dice in the Board" + rolldice);
+			int player1=dice.calculatePlayerValue(player, rolldice);
+			System.out.println("The position of player is"+player1);
+			
+		}
+        
+		public  int roll() {
+          int diceRoll = (int)(Math.random()*6 )+1 ;
+          return diceRoll;
+        }
+		public int calculatePlayerValue(int player, int diceValue)
+		{
+		    player = player + diceValue;
+		      
+		    if(player > winpoint)
+		    {
+		        player = player - diceValue;
+		        return player;
+		    }
+		      
+		    if(null!=snake.get(player))
+		    {
+		        System.out.println("Move behind by ladder");
+		        player= snake.get(player);
+		    }
+		      
+		    if(null!=ladder.get(player))
+		    {
+		        System.out.println("climb up the ladder");
+		        player= ladder.get(player);
+		    }
+		    System.out.println(player);
+		    return player;
+		}
+        
 
-}
+          
+        }
+
+	
+	
+    
+    
+    
+
+    
+       
+
+        
