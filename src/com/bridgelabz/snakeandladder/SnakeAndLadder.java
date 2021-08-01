644ce97bece0da7f05120c9a9d5d062f8ee5db94
pra152 <pravatdas211@gmail.com>
2021-08-01 08:34:05 +0530

    Added :Snake and ladder game in single player

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeandladder;
+
+import java.util.Scanner;
+
+public class SnakeAndLadder {
+	private static final Scanner sc=new Scanner(System.in);
+	int players_position=0;
+	public static void main(String []args) {
+		System.out.println("Enter how many players:");
+		int players=sc.nextInt();
+	}
+
+}
