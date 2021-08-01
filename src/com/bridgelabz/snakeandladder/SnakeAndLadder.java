e39637355e61777862b9691d0dbbb99601005fc8
pra152 <pravatdas211@gmail.com>
2021-08-01 09:42:44 +0530

    Added: Random() function to roll the dice

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,13 +1,29 @@
 package com.bridgelabz.snakeandladder;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class SnakeAndLadder {
-	private static final Scanner sc=new Scanner(System.in);
-	int players_position=0;
-	public static void main(String []args) {
-		System.out.println("Enter how many players:");
-		int players=sc.nextInt();
-	}
-
+        public static void main(String[] args) {
+        	SnakeAndLadder dice=new SnakeAndLadder();
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter Fisrt Player:");
+			int player1=sc.nextInt();
+			int rolldice=dice.roll();
+			System.out.println("Roll the dice in the Board" + rolldice);
+		}
+        public static int roll() {
+          int diceRoll = (int)(Math.random()*6 )+1 ;
+          return diceRoll;
+        }
 }
+	
+	
+    
+    
+    
+
+    
+       
+
+        
